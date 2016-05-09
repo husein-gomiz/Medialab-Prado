b1305a113e449fe19ef6dd5414ba4b8c560d890e
husein-gomiz <husein.gomiz@gmail.com>
2016-05-09 21:07:07 +0200

    smooth_peack_detect: upgrade code with comments and better variable names

--- smooth_peack_detect/smooth_peack_detect.ino
+++ smooth_peack_detect/smooth_peack_detect.ino
@@ -1,35 +1,48 @@
+/******************************************************
+ * This program reads an analog input, smoothens it and detects peacks over a
+ * threshold. When a peack is detected a servo motor is placed at min or max 
+ * position depending on its previous state
+ */
+
+
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
 
-int inputPin  = 0;  // analog pin used to connect the potentiometer
-int val,val2;    // variable to read the value from the analog pin
 
+
+//Smoothing variables
 const int numReadings = 10;
 int readings[numReadings];      // the readings from the analog input
 int readIndex = 0;              // the index of the current reading
 int total = 0;                  // the running total
 int average = 0;                // the average
-int off=1,on=0, peack=0,reading=0, over_threshold;
-int thres_top=0, thres_bot=0;
+
+int inputPin  = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
+int open_servo=1,closed_servo=0, peack=0,reading=0, over_threshold;
+int thres_max, thres_min;  //threshold limits
+const int closed_val= 170;
+const int open_val=20 ;
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
   Serial.begin(9600);
+  
   for (int thisReading = 0; thisReading < numReadings; thisReading++) {
-    readings[thisReading] = 0;
+    readings[thisReading] = 0; //initialize array
   }
-  thres_top=800;  //umbral superior
-  thres_bot=600;  //umbral inferior
+  thres_max=800;  //upper threshold
+  thres_min=600;  //bottom  threshold
 
 }
 
 void loop() {
 
-//Smoothing ( suaviza la señal para quitar ruido)
+//Smoothing 
 //***********************************************************
 
-      // subtract the last reading:
+  // subtract the last reading:
   total = total - readings[readIndex];
   // read from the sensor:
   readings[readIndex] = analogRead(inputPin);
@@ -50,34 +63,33 @@ void loop() {
 //****************************************************************
 
   
-//Si el pico sube del umbral superior, se queda en espera de bajar del umbral inferior.
-  if (average>800) {
+//if signal exceeds the top threshold it waits for it to pass the bottom threshold to declare a peack
+  if (average>thres_max) {
     over_threshold=1;
   }
-
-// en cuanto pasa el umbral inferior detecta un pico
-  if (over_threshold ==1 && average < 600){
+  
+  if (over_threshold ==1 && average < thres_min){
     peack=1;
     over_threshold=0;
    // delay(1500);
   }
 
-// si ha habido un pico, si esta abierto => cerrar y viceversa.  
+// when a peack occurs, close the servo if open and viceversa 
   if (peack==1) {
-    if (off==1) {
-      val2=170;
-      on = 1;
-      off = 0;
+    if (open_servo==1) {
+      val=closed_val;
+      closed_servo = 1;
+      open_servo = 0;
       peack = 0;
      }
     else{
-      val2=20;
-      off=1;
-      on=0;
+      val=open_val;
+      open_servo=1;
+      closed_servo=0;
       peack=0;
     }
   }
   Serial.println(average);
-  myservo.write(val2);                  // sets the servo position according to the  value
+  myservo.write(val);                  // sets the servo position according to the  value
   delay(15);                           // waits for the servo to get there
 }
